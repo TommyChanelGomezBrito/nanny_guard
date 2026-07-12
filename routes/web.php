19ff5b97783cbffeb1f960d4f0c5e56914209231
tommychanelgomezbrito <tommygomeznuovo@outlook.it>
2026-07-12 23:37:37 +0200

    Add GET routes for login/register/password forms

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,13 @@ Route::middleware('auth')->group(function () {
     });
 });
 
+Route::middleware('guest')->group(function () {
+    Route::view('/login', 'auth.login')->name('login');
+    Route::view('/register', 'auth.register')->name('register');
+    Route::view('/forgot-password', 'auth.passwords.email')->name('password.request');
+    Route::view('/reset-password/{token}', 'auth.passwords.reset')->name('password.reset');
+});
+
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest')
     ->name('register');
